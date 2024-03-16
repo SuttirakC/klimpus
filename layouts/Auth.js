e0b60110a073d239ec6bf9455d5855d434a9e4a1
FRIEND\Sutti <suttirak.egat@gmail.com>
2024-03-17 02:40:52 +0700

    fix Bg

--- layouts/Auth.js
+++ layouts/Auth.js
@@ -12,9 +12,10 @@ export default function Auth({ children }) {
       <main>
         <section className="relative w-full h-full py-40 min-h-screen ">
           <div
-            className="absolute top-0 w-full h-screen bg-no-repeat bg-center bg-cover brightness-50"
+            className="absolute top-0 w-full h-screen bg-no-repeat bg-center bg-cover"
             style={{
               backgroundImage: "url('/img/bg-lib.jpg')",
+              filter: "brightness(50%)", // Adjust brightness to darken the image
             }}
           ></div>
           {children}
