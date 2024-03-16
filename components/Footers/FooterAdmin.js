ab7309527a3ffba707dda7c3dfaf1cc492767d09
FRIEND\Sutti <suttirak.egat@gmail.com>
2024-03-17 03:17:13 +0700

    Back to Update ChartJS

--- components/Footers/FooterAdmin.js
+++ components/Footers/FooterAdmin.js
@@ -8,17 +8,51 @@ export default function FooterAdmin() {
           <hr className="mb-4 border-b-1 border-slate-200" />
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4">
-              <div className="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left">
-              KMUTT Library Monitoring Public Utility Service
-              </div>
-            </div>
-            <div className="w-full md:w-6/12 px-4">
-            <div className="text-sm text-blueGray-500 font-semibold text-center">
-                © {new Date().getFullYear()}{" "}
-                <a>
-                  klimpus.kmutt.ac.th  is a subsidiary website of KMUTT. All rights reserved.
+              <div className="text-sm text-slate-500 font-semibold py-1 text-center md:text-left">
+                Copyright © {new Date().getFullYear()}{" "}
+                <a
+                  href="https://www.creative-tim.com?ref=nnjs-footer-admin"
+                  className="text-slate-500 hover:text-slate-700 text-sm font-semibold py-1"
+                >
+                  Creative Tim
                 </a>
               </div>
+            </div>
+            <div className="w-full md:w-8/12 px-4">
+              <ul className="flex flex-wrap list-none md:justify-end  justify-center">
+                <li>
+                  <a
+                    href="https://www.creative-tim.com?ref=nnjs-footer-admin"
+                    className="text-slate-600 hover:text-slate-800 text-sm font-semibold block py-1 px-3"
+                  >
+                    Creative Tim
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.creative-tim.com/presentation?ref=nnjs-footer-admin"
+                    className="text-slate-600 hover:text-slate-800 text-sm font-semibold block py-1 px-3"
+                  >
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="http://blog.creative-tim.com?ref=nnjs-footer-admin"
+                    className="text-slate-600 hover:text-slate-800 text-sm font-semibold block py-1 px-3"
+                  >
+                    Blog
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/creativetimofficial/notus-nextjs/blob/main/LICENSE.md?ref=nnjs-footer-admin"
+                    className="text-slate-600 hover:text-slate-800 text-sm font-semibold block py-1 px-3"
+                  >
+                    MIT License
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
