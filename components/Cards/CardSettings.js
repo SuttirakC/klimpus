5ef18f9444e342940ce545b949cad5b1c3fd42b2
pimpitchaya <pimpitchaya.st@gmail.com>
2024-03-22 03:46:38 +0700

    update account control page

--- components/Cards/CardSettings.js
+++ components/Cards/CardSettings.js
@@ -5,10 +5,10 @@ import React from "react";
 export default function CardSettings() {
   return (
     <>
-      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-slate-100 border-0">
-        <div className="rounded-t bg-white mb-0 px-6 py-6">
+      <div className="relative flex flex-col w-full break-words w-full mb-6 shadow-lg rounded-2xl bg-slate-100 border-0">
+        <div className="rounded-2xl bg-white mb-0 px-6 py-6">
           <div className="text-center flex justify-between">
-            <h6 className="text-slate-700 text-xl font-bold">My account</h6>
+            <h6 className="text-slate-700 text-xl font-bold">Account Control</h6>
             <button
               className="bg-slate-700 active:bg-slate-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
               type="button"
--- components/Headers/HeaderStats.js
+++ components/Headers/HeaderStats.js
@@ -8,59 +8,89 @@ export default function HeaderStats() {
   return (
     <>
       {/* Header */}
-      <div className="relative bg-slate-800 md:pt-32 pb-32 pt-12">
+      <div className="relative md:pt-32 pb-32 pt-12">
         <div className="px-4 md:px-10 mx-auto w-full">
           <div>
             {/* Card stats */}
             <div className="flex flex-wrap">
               <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
                 <CardStats
-                  statSubtitle="TRAFFIC"
-                  statTitle="350,897"
-                  statArrow="up"
-                  statPercent="3.48"
-                  statPercentColor="text-emerald-500"
-                  statDescripiron="Since last month"
-                  statIconName="far fa-chart-bar"
-                  statIconColor="bg-red-500"
-                />
-              </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="NEW USERS"
-                  statTitle="2,356"
-                  statArrow="down"
-                  statPercent="3.48"
-                  statPercentColor="text-red-500"
-                  statDescripiron="Since last week"
-                  statIconName="fas fa-chart-pie"
-                  statIconColor="bg-orange-500"
-                />
-              </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="SALES"
-                  statTitle="924"
-                  statArrow="down"
-                  statPercent="1.10"
-                  statPercentColor="text-orange-500"
-                  statDescripiron="Since yesterday"
+                  statSubtitle="Total Account"
+                  statTitle="003"
+                  // statArrow="up"
+                  // statPercent="3.48"
+                  // statPercentColor="text-emerald-500"
+                  // statDescripiron="Since last month"
                   statIconName="fas fa-users"
-                  statIconColor="bg-pink-500"
+                  statIconColor="bg-gray-300"
                 />
               </div>
               <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
                 <CardStats
-                  statSubtitle="PERFORMANCE"
-                  statTitle="49,65%"
-                  statArrow="up"
-                  statPercent="12"
-                  statPercentColor="text-emerald-500"
-                  statDescripiron="Since last month"
-                  statIconName="fas fa-percent"
-                  statIconColor="bg-sky-500"
+                  statSubtitle="Admin Role"
+                  statTitle="002"
+                  // statArrow="down"
+                  // statPercent="3.48"
+                  // statPercentColor="text-red-500"
+                  // statDescripiron="Since last week"
+                  statIconName="fas fa-user-lock"
+                  statIconColor="bg-gray-300"
                 />
               </div>
+              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+                <CardStats
+                  statSubtitle="Technical Role"
+                  statTitle="001"
+                  // statArrow="down"
+                  // statPercent="1.10"
+                  // statPercentColor="text-orange-500"
+                  // statDescripiron="Since yesterday"
+                  statIconName="fas fa-user-gear"
+                  statIconColor="bg-gray-300"
+                />
+              </div>
+              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+                <div className="relative flex flex-col min-w-0 break-words bg-white rounded-3xl mb-6 xl:mb-0 shadow-lg">
+                  <div className="flex-auto p-4">
+                    <div className="flex flex-wrap">
+                      <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
+                        <h5 className="text-slate-400 uppercase font-bold text-md">
+                          Create Account
+                        </h5>
+                        <span className="font-semibold text-2xl text-kmutt_blue-100 ">
+                          Create New Account
+                        </span>
+                      </div>
+                      <div className="relative w-auto pl-4 flex-initial">
+                        <div
+                          className={
+                            "text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full " +
+                            "bg-gray-300"
+                          }
+                        >
+                          <i className={"fas fa-user-plus"}></i>
+                        </div>
+                      </div>
+                    </div>
+                    {/* <p className="text-sm text-slate-400 mt-4">
+            <span className={statPercentColor + " mr-2"}>
+              <i
+                className={
+                  statArrow === "up"
+                    ? "fas fa-arrow-up"
+                    : statArrow === "down"
+                    ? "fas fa-arrow-down"
+                    : ""
+                }
+              ></i>{" "}
+              {statPercent}%
+            </span>
+            <span className="whitespace-nowrap">{statDescripiron}</span>
+          </p> */}
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
