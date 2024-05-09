a2f0659bc2183c59ea0e8cc4e0b0b212fb0e744d
FRIEND\Sutti <suttirak.egat@gmail.com>
2024-05-09 15:53:28 +0700

    fix account control

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -3,7 +3,7 @@ import { query } from '../../../lib/db';
 export default async function handler(req, res) {
     try {
         if (req.method === 'POST') {
-            // console.log('reqBody', req.body);
+            const bcrypt = require('bcrypt');
             const username = req.body.username;
             const password = req.body.password;
 
@@ -18,12 +18,6 @@ export default async function handler(req, res) {
                 query: 'SELECT * FROM users WHERE username = ?',
                 values: [username],
             });
-
-            const bcrypt = require('bcrypt');
-            // Load hash from your password DB.
-            // bcrypt.compare(password, user[0].password, function (err, result) {
-            //     // result == true
-            // });
 
             if (user.length === 0) {
                 res.status(400).json({ response: { message: 'User not found' } });
@@ -40,29 +34,6 @@ export default async function handler(req, res) {
             res.status(200).json({ response: { message: 'ok', user: user[0] } });
         }
 
-
-        // const addUser = await query({
-        //     query: 'INSERT INTO users (username, password, email, role_id, firstname, lastname) VALUES (?, ?, ?, ?, ?, ?)',
-        //     values: [username, hashedPassword, email, role_id, firstname, lastname],
-        // });
-
-        // if (addUser.affectedRows) {
-        //     message = 'Created Successfully';
-        // } else {
-        //     message = 'Creation failed';
-        // }
-
-        // let userData = {
-        //     // user_id: addUser.affectedRows,
-        //     username: username,
-        //     password: password,
-        //     email: email,
-        //     role_id: role_id,
-        //     firstname: firstname,
-        //     lastname: lastname
-        // }
-
-        // res.status(200).json({ response: { message: message, user: userData } });
     } catch (error) {
         res.status(400).json({ response: { message: error.message } });
     }
