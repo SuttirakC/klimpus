85f89c9bf0cddb76dc9a85a466b9829dc33980e0
FRIEND\Sutti <suttirak.egat@gmail.com>
2024-05-23 15:22:18 +0700

    fix notifications color

--- components/Cards/CardNotiChiller.js
+++ components/Cards/CardNotiChiller.js
@@ -16,7 +16,7 @@ export default function CardNotiChiller({ notifications }) {
                 <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                     {notifications.length > 0 ? (
                         notifications.map((noti, index) => (
-                            <div key={index} role="alert" className={`mt-4 alert bg-kmutt_${noti.noti_case_level === 1 ? 'yellow' : 'red'}-100`}>
+                            <div key={index} role="alert" className={`mt-4 alert bg-kmutt_${noti.noti_case_level === 1 ? 'yellow-100' : 'red-200'}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
                                     {noti.noti_case_level === 1 ? (
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
